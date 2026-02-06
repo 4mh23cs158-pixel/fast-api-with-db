aa5b7ee68081b1498b4d5cfda542f906c268471f
4mh23cs158-pixel <4mh23cs158@gmail.com>
2026-02-06 15:56:47 +0530

    grow

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -1,11 +1,17 @@
 from fastapi import APIRouter
+from sqlalchemy.orm import Session
+from db import get_db
+from fastapi import Depends
+from repositories.user_repo import UserRepo
 
 router=APIRouter()
 
 @router.post("/signup")
-def signup():
-    return {"message":"signup SUCCESSFULL"}
-
+def signup(db:Session=Depends(get_db)):
+    user_repo=UserRepo(db)
+    user_repo.add_user()
+    return{"message":"user added successfully"}
+    
 @router.post("/login")
 def login():
     return {"message":"login SUCCESSFULL"}
